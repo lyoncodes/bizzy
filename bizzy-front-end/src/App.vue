8cba30070d9fa6ba585d7cea76a7d194d4e79024
Michael Lyon <lyoncodes@gmail.com>
2020-11-13 01:52:06 -0800

    initial commit, scaffolds front-end, adds boiler copy & .svg

---
+++ bizzy-front-end/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" class="header-logo" src="./assets/money-rocket.svg">
+    <HelloWorld msg="Hi, I'm Michael"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+.header-logo {
+  height: 250px;
+}
+</style>
